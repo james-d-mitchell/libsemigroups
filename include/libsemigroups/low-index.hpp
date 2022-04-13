1e41ebe6ccca78240dca3158203058a7ea346924
James D. Mitchell <jdm3@st-andrews.ac.uk>
2022-04-13 12:10:01 +0100

    Checkpoint removed coset manager

--- include/libsemigroups/low-index.hpp
+++ include/libsemigroups/low-index.hpp
@@ -25,7 +25,6 @@
 #include <cstddef>
 
 #include "collapsible-word-graph.hpp"
-#include "coset.hpp"
 #include "digraph.hpp"
 #include "felsch-tree.hpp"
 #include "present.hpp"
@@ -47,35 +46,41 @@ namespace libsemigroups {
     }
   }  // namespace
 
-  class LowIndexCongruences : public detail::CosetManager {
-   public:
-    using Deduction = std::pair<coset_type, letter_type>;
+  class LowIndexCongruences {
+    using node_type   = uint32_t;
+    using letter_type = uint16_t;
+    using size_type   = size_t;  // TODO use WordGraph::size_type instead
+
+   private:
     struct Edge {
-      Edge(coset_type s, letter_type g, coset_type t, size_t e, size_t n)
+      Edge(node_type s, letter_type g, node_type t, size_t e, size_t n)
           : source(s), generator(g), target(t), num_edges(e), num_nodes(n) {}
-      coset_type  source;
+      node_type   source;
       letter_type generator;
-      coset_type  target;
+      node_type   target;
       size_t num_edges;  // Number of edges in the graph when *this was added to
                          // the stack
       size_t num_nodes;  // Same as above but for nodes
     };
-    using PendingDefs = std::vector<Edge>;
-    using Deductions  = std::vector<Deduction>;
 
-   private:
-    Deductions                     _deduct;
+    using Definition         = std::pair<node_type, letter_type>;
+    using Definitions        = std::vector<Definition>;
+    using PendingDefinitions = std::vector<Edge>;
+
+    Definitions                    _definitions;
     FelschTree                     _felsch_tree;
     CollapsibleWordGraph<uint32_t> _word_graph;
     Presentation<word_type>        _presentation;
+    size_type                      _num_active_nodes;
     // TODO stats?
 
    public:
     LowIndexCongruences(Presentation<word_type> const& p)
-        : _deduct(),
+        : _definitions(),
           _felsch_tree(p.alphabet().size()),
           _word_graph(),
-          _presentation(p) {
+          _presentation(p),
+          _num_active_nodes(1) {
       _felsch_tree.add_relations(_presentation.cbegin(), _presentation.cend());
       _word_graph.add_nodes(1);
       _word_graph.add_to_out_degree(_presentation.alphabet().size());
@@ -85,33 +90,24 @@ namespace libsemigroups {
       return _word_graph;
     }
 
-    void reserve(size_t n) {
-      size_t m = coset_capacity();
-      if (n > m) {
-        m = n - m;
-        _word_graph.add_nodes(m);
-        add_free_cosets(m);
-      }
-    }
-
-    inline bool push_definition_felsch(coset_type const& c, size_t i) noexcept {
+    inline bool push_definition_felsch(node_type const& c, size_t i) noexcept {
       auto j = (i % 2 == 0 ? i + 1 : i - 1);
       return push_definition_felsch(
           c, *(_presentation.cbegin() + i), *(_presentation.cbegin() + j));
     }
 
-    bool push_definition_felsch(coset_type const& c,
-                                word_type const&  u,
-                                word_type const&  v) noexcept {
-      LIBSEMIGROUPS_ASSERT(is_active_coset(c));
+    bool push_definition_felsch(node_type const& c,
+                                word_type const& u,
+                                word_type const& v) noexcept {
+      LIBSEMIGROUPS_ASSERT(c < _num_active_nodes);
       LIBSEMIGROUPS_ASSERT(!u.empty());
       LIBSEMIGROUPS_ASSERT(!v.empty());
-      coset_type x = action_digraph_helper::follow_path_nc(
+      node_type x = action_digraph_helper::follow_path_nc(
           _word_graph, c, u.cbegin(), u.cend() - 1);
       if (x == UNDEFINED) {
         return true;
       }
-      coset_type y = action_digraph_helper::follow_path_nc(
+      node_type y = action_digraph_helper::follow_path_nc(
           _word_graph, c, v.cbegin(), v.cend() - 1);
       if (y == UNDEFINED) {
         return true;
@@ -120,14 +116,14 @@ namespace libsemigroups {
     }
 
     bool
-    push_definition(coset_type x, letter_type a, coset_type y, letter_type b) {
-      LIBSEMIGROUPS_ASSERT(is_valid_coset(x));
-      LIBSEMIGROUPS_ASSERT(is_valid_coset(y));
+    push_definition(node_type x, letter_type a, node_type y, letter_type b) {
+      LIBSEMIGROUPS_ASSERT(x < _num_active_nodes);
+      LIBSEMIGROUPS_ASSERT(y < _num_active_nodes);
       LIBSEMIGROUPS_ASSERT(a < _presentation.alphabet().size());
       LIBSEMIGROUPS_ASSERT(b < _presentation.alphabet().size());
 
-      coset_type const xa = _word_graph.unsafe_neighbor(x, a);
-      coset_type const yb = _word_graph.unsafe_neighbor(y, b);
+      node_type const xa = _word_graph.unsafe_neighbor(x, a);
+      node_type const yb = _word_graph.unsafe_neighbor(y, b);
 
       if (xa == UNDEFINED && yb != UNDEFINED) {
         def_edge(x, a, yb);
@@ -144,24 +140,14 @@ namespace libsemigroups {
       return _presentation;
     }
 
-    coset_type new_coset() {
-      if (has_free_cosets()) {
-        coset_type const c = new_active_coset();
-        // Clear the new coset's row in each table
-        _word_graph.clear_sources_and_targets(c);
-        return c;
-      } else {
-        reserve(2 * coset_capacity());
-        return new_active_coset();
-      }
-    }
-
-    bool process_deductions(size_t start) {
-      for (size_t i = start; i < _deduct.size(); ++i) {
-        auto const& d = _deduct[i];
-        if (is_active_coset(d.first)) {
+    bool process_definitionsions(size_t start) {
+      for (size_t i = start; i < _definitions.size(); ++i) {
+        auto const& d = _definitions[i];
+        if (d.first
+            < _num_active_nodes) {  // TODO try removing this check, think it
+                                    // always has to be the case
           _felsch_tree.push_back(d.second);
-          if (!process_deductions_dfs_v1(d.first)) {
+          if (!process_definitionsions_dfs_v1(d.first)) {
             return false;
           }
         }
@@ -169,7 +155,7 @@ namespace libsemigroups {
       return true;
     }
 
-    bool process_deductions_dfs_v1(coset_type c) {
+    bool process_definitionsions_dfs_v1(node_type c) {
       for (auto it = _felsch_tree.cbegin(); it < _felsch_tree.cend(); ++it) {
         if (!push_definition_felsch(c, *it)) {
           return false;
@@ -179,9 +165,9 @@ namespace libsemigroups {
       size_t const n = _presentation.alphabet().size();
       for (size_t x = 0; x < n; ++x) {
         if (_felsch_tree.push_front(x)) {
-          coset_type e = _word_graph.first_source(c, x);
+          node_type e = _word_graph.first_source(c, x);
           while (e != UNDEFINED) {
-            if (!process_deductions_dfs_v1(e)) {
+            if (!process_definitionsions_dfs_v1(e)) {
               return false;
             }
             e = _word_graph.next_source(e, x);
@@ -192,72 +178,74 @@ namespace libsemigroups {
       return true;
     }
 
-    inline void def_edge(coset_type c, letter_type x, coset_type d) noexcept {
-      LIBSEMIGROUPS_ASSERT(is_valid_coset(c));
+    inline void def_edge(node_type c, letter_type x, node_type d) noexcept {
+      LIBSEMIGROUPS_ASSERT(c < _num_active_nodes);
       LIBSEMIGROUPS_ASSERT(x < _presentation.alphabet().size());
-      LIBSEMIGROUPS_ASSERT(is_valid_coset(d));
-      _deduct.emplace_back(c, x);
+      LIBSEMIGROUPS_ASSERT(d < _num_active_nodes);
+      _definitions.emplace_back(c, x);
       _word_graph.add_edge_nc(c, d, x);
     }
 
     // Returns the number of right congruences with up to n (inclusive)
     // classes.
     size_t number_of_congruences(size_t n) {
-      size_t      nr = 0;
-      PendingDefs pending;
-      pending.push_back(Edge(0, 0, UNDEFINED, 0, 1));
-      pending.push_back(Edge(0, 0, 0, 0, 1));
+      if (_word_graph.number_of_nodes() < n) {
+        _word_graph.add_nodes(n - _word_graph.number_of_nodes());
+      }
+
+      size_t             nr = 0;
+      PendingDefinitions pending;
+      pending.emplace_back(0, 0, UNDEFINED, 0, 1);
+      pending.emplace_back(0, 0, 0, 0, 1);
+
       while (!pending.empty()) {
       dive:
         auto current = pending.back();
         pending.pop_back();
 
         // Backtrack if necessary
-        while (_deduct.size() > current.num_edges) {
-          auto const& p = _deduct.back();
+        while (_definitions.size() > current.num_edges) {
+          auto const& p = _definitions.back();
           _word_graph.remove_edge_nc(p.first, p.second);
-          _deduct.pop_back();
+          _definitions.pop_back();
         }
-        while (number_of_cosets_active() > current.num_nodes) {
-          free_coset(number_of_cosets_active() - 1);
-        }
+
+        _num_active_nodes = current.num_nodes;
 
         LIBSEMIGROUPS_ASSERT(
             _word_graph.unsafe_neighbor(current.source, current.generator)
             == UNDEFINED);
         {
-          size_t start = _deduct.size();
+          size_t start = _definitions.size();
 
           if (current.target != UNDEFINED) {
             def_edge(current.source, current.generator, current.target);
           } else {
-            if (number_of_cosets_active() == n) {
+            if (_num_active_nodes == n) {
               continue;
             }
-            def_edge(current.source, current.generator, new_coset());
+            def_edge(current.source, current.generator, _num_active_nodes++);
           }
 
-          if (process_deductions(start)) {
-            coset_type  next = current.source;
-            letter_type a    = current.generator + 1;
-            while (next != first_free_coset()) {
+          if (process_definitionsions(start)) {
+            letter_type a = current.generator + 1;
+            for (node_type next = current.source; next < _num_active_nodes;
+                 ++next) {
               for (; a < _presentation.alphabet().size(); ++a) {
                 if (_word_graph.unsafe_neighbor(next, a) == UNDEFINED) {
                   pending.emplace_back(next,
                                        a,
                                        UNDEFINED,
-                                       _deduct.size(),
-                                       number_of_cosets_active());
-                  for (coset_type b = _id_coset; b != first_free_coset();
-                       b            = next_active_coset(b)) {
+                                       _definitions.size(),
+                                       _num_active_nodes);
+                  for (node_type b = 0; b < _num_active_nodes; ++b) {
                     pending.emplace_back(
-                        next, a, b, _deduct.size(), number_of_cosets_active());
+                        next, a, b, _definitions.size(), _num_active_nodes);
                   }
                   goto dive;
                 }
               }
-              next = next_active_coset(next);
-              a    = 0;
+              a = 0;
             }
             // No undefined edges, word graph is complete
             // check_compatibility(_word_graph, _presentation);
