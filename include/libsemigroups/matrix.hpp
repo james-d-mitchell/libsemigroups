232dc73f4cb3ea6f68f70ed663b3179a9dbbbff0
James D. Mitchell <jdm3@st-andrews.ac.uk>
2025-03-07 14:42:42 +0000

    More classification of TODOs

--- include/libsemigroups/matrix.hpp
+++ include/libsemigroups/matrix.hpp
@@ -6591,26 +6591,22 @@ namespace libsemigroups {
       // Comparison operators
       ////////////////////////////////////////////////////////////////////////
 
-      // TODO template
       bool operator==(ProjMaxPlusMat const& that) const {
         normalize();
         that.normalize();
         return _underlying_mat == that._underlying_mat;
       }
 
-      // TODO template
       bool operator!=(ProjMaxPlusMat const& that) const {
         return !(_underlying_mat == that._underlying_mat);
       }
 
-      // TODO template
       bool operator<(ProjMaxPlusMat const& that) const {
         normalize();
         that.normalize();
         return _underlying_mat < that._underlying_mat;
       }
 
-      // TODO template
       bool operator>(ProjMaxPlusMat const& that) const {
         return that < *this;
       }
