fef252a656da97d9f9c44dadc108888cfb431459
James Mitchell <jdm3@st-andrews.ac.uk>
2026-03-31 16:29:52 +0100

    Separate Rule/Rules into separate file

---
+++ include/libsemigroups/detail/rules.hpp
@@ -0,0 +1,257 @@
+//
+// libsemigroups - C++ library for semigroups and monoids
+// Copyright (C) 2026 Joseph Edwards + James D. Mitchell
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// This file contains the implementation of a Rule object containers for Rule
+// objects. It also includes rewriter classes that can be used to rewrite
+// strings relative to a collection of rules.
+
+#ifndef LIBSEMIGROUPS_DETAIL_RULES_HPP_
+#define LIBSEMIGROUPS_DETAIL_RULES_HPP_
+
+#include <string>
+
+namespace libsemigroups {
+  namespace detail {
+    ////////////////////////////////////////////////////////////////////////
+    // Rule
+    ////////////////////////////////////////////////////////////////////////
+
+    class Rule {
+     public:
+#ifdef LIBSEMIGROUPS_DEBUG
+      enum class State : uint8_t { active = 0, inactive = 1, pending = 2 };
+#endif
+
+      using native_word_type = std::string;
+
+     private:
+      native_word_type _lhs;
+      native_word_type _rhs;
+#ifdef LIBSEMIGROUPS_DEBUG
+      State _state;
+#endif
+
+     public:
+      Rule()
+          : _lhs(),
+            _rhs()
+#ifdef LIBSEMIGROUPS_DEBUG
+            ,
+            _state(Rule::State::inactive)
+#endif
+      {
+      }
+
+      Rule& operator=(Rule const& copy) = delete;
+      Rule(Rule const& copy)            = delete;
+      Rule(Rule&& copy)                 = delete;
+      Rule& operator=(Rule&& copy)      = delete;
+
+      ~Rule() = default;
+
+      [[nodiscard]] native_word_type const& lhs() const noexcept {
+        return _lhs;
+      }
+
+      [[nodiscard]] native_word_type const& rhs() const noexcept {
+        return _rhs;
+      }
+
+      [[nodiscard]] native_word_type& lhs() noexcept {
+        return _lhs;
+      }
+
+      [[nodiscard]] native_word_type& rhs() noexcept {
+        return _rhs;
+      }
+
+#ifdef LIBSEMIGROUPS_DEBUG
+      [[nodiscard]] State state() const noexcept {
+        return _state;
+      }
+
+      Rule& state(State val) {
+        _state = val;
+        return *this;
+      }
+#endif
+    };  // class Rule
+
+    template <typename ReductionOrder>
+    void reorder(Rule* rule) {
+      if (ReductionOrder{}(rule->lhs(), rule->rhs())) {
+        std::swap(rule->lhs(), rule->rhs());
+      }
+    }
+
+    ////////////////////////////////////////////////////////////////////////
+    // Rules
+    ////////////////////////////////////////////////////////////////////////
+
+    class Rules {
+     public:
+      using iterator               = std::list<Rule*>::iterator;
+      using const_iterator         = std::list<Rule*>::const_iterator;
+      using const_reverse_iterator = std::list<Rule*>::const_reverse_iterator;
+
+     private:
+      struct Stats {
+        Stats() noexcept;
+        Stats& init() noexcept;
+
+        Stats(Stats const&) noexcept            = default;
+        Stats(Stats&&) noexcept                 = default;
+        Stats& operator=(Stats const&) noexcept = default;
+        Stats& operator=(Stats&&) noexcept      = default;
+
+        size_t   max_active_rules;
+        size_t   max_length_lhs_rule;
+        size_t   max_pending_rules;  // TODO(1) rename
+        size_t   min_length_lhs_rule;
+        uint64_t total_rules;
+      };
+
+      std::list<Rule*>        _active_rules;
+      std::array<iterator, 2> _cursors;  // TODO(1) rm?
+      std::list<Rule*>        _inactive_rules;
+      std::vector<Rule*>      _pending_rules;
+      mutable Stats           _stats;
+
+      // TODO(1) try maintaining pending_rules as a heap?
+
+      void init_cursors();
+
+     public:
+      ////////////////////////////////////////////////////////////////////////
+      // Constructors and initializers
+      ////////////////////////////////////////////////////////////////////////
+
+      Rules();
+      Rules& init();
+
+      // This is currently not used anywhere, because we go through the
+      // copy/move assignment operators (so no RewritingSystem calls these
+      // functions in their copy/move constructors).
+      Rules(Rules const& that) : Rules() {
+        *this = that;
+      }
+
+      // This is currently not used anywhere, because we go through the
+      // copy/move assignment operators (so no RewritingSystem calls these
+      // functions in their copy/move constructors).
+      Rules(Rules&& that) : Rules() {
+        *this = std::move(that);
+      }
+
+      Rules& operator=(Rules const&);
+      Rules& operator=(Rules&& that);
+
+      ~Rules();
+
+      ////////////////////////////////////////////////////////////////////////
+      // Adding/modifying rules
+      ////////////////////////////////////////////////////////////////////////
+
+      template <typename Iterator>
+      Rule* add_pending_rule(Iterator first1,
+                             Iterator last1,
+                             Iterator first2,
+                             Iterator last2) {
+        return add_pending_rule(new_rule(first1, last1, first2, last2));
+      }
+
+      void add_active_rule(Rule* rule);
+
+      void add_inactive_rule(Rule* rule) {
+#ifdef LIBSEMIGROUPS_DEBUG
+        rule->state(Rule::State::inactive);
+#endif
+        _inactive_rules.push_back(rule);
+      }
+
+      void sort_pending_rules();
+
+      [[nodiscard]] iterator make_active_rule_pending(iterator it);
+
+      ////////////////////////////////////////////////////////////////////////
+      // Getting rules
+      ////////////////////////////////////////////////////////////////////////
+
+      [[nodiscard]] std::list<Rule*> const& active_rules() const noexcept {
+        return _active_rules;
+      }
+
+      [[nodiscard]] std::list<Rule*>& active_rules() noexcept {
+        return _active_rules;
+      }
+
+      [[nodiscard]] std::vector<Rule*> const& pending_rules() const noexcept {
+        return _pending_rules;
+      }
+
+      [[nodiscard]] std::vector<Rule*>& pending_rules() noexcept {
+        return _pending_rules;
+      }
+
+      [[nodiscard]] Rule* pop_pending_rule();
+
+      // TODO(1) remove?
+      [[nodiscard]] iterator& cursor(size_t index) {
+        LIBSEMIGROUPS_ASSERT(index < _cursors.size());
+        return _cursors[index];
+      }
+
+      ////////////////////////////////////////////////////////////////////////
+      // Numbers of rules
+      ////////////////////////////////////////////////////////////////////////
+
+      // TODO rm? just use active_rules().size()
+      [[nodiscard]] size_t number_of_active_rules() const noexcept {
+        return _active_rules.size();
+      }
+
+      [[nodiscard]] size_t number_of_inactive_rules() const noexcept {
+        return _inactive_rules.size();
+      }
+
+      // TODO rm? just use pending_rules().size()
+      [[nodiscard]] size_t number_of_pending_rules() const noexcept {
+        return _pending_rules.size();
+      }
+
+      [[nodiscard]] Stats const& stats() const {
+        return _stats;
+      }
+
+      // TODO helper
+      [[nodiscard]] size_t max_length_lhs_active_rule() const;
+
+     private:
+      Rule* add_pending_rule(Rule* rule);
+
+      [[nodiscard]] Rule* copy_rule(Rule const* rule);
+      [[nodiscard]] Rule* new_rule();
+
+      template <typename Iterator>
+      [[nodiscard]] Rule* new_rule(Iterator first1,
+                                   Iterator last1,
+                                   Iterator first2,
+                                   Iterator last2);
+    };  // class Rules
+
+  }  // namespace detail
+}  // namespace libsemigroups
