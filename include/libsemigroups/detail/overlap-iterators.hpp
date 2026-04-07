b0fde4fbcc1a85da7f62a44a98b57c60f5c72d70
James Mitchell <jdm3@st-andrews.ac.uk>
2026-04-07 08:30:52 +0100

    TODO -> DONE

--- include/libsemigroups/detail/overlap-iterators.hpp
+++ include/libsemigroups/detail/overlap-iterators.hpp
@@ -18,41 +18,34 @@
 #ifndef LIBSEMIGROUPS_DETAIL_OVERLAP_ITERATORS_HPP_
 #define LIBSEMIGROUPS_DETAIL_OVERLAP_ITERATORS_HPP_
 
-#include <string_view>    // for string_view
-#include <unordered_map>  // for unordered_map
-
-#include "libsemigroups/config.hpp"  // for LIBSEMIGROUPS_DEBUG
-#include "libsemigroups/debug.hpp"   // for LIBSEMIGROUPS_ASSERT
+#include <cstddef>   // for size_t
+#include <iterator>  // for forward_iterator_tag
+#include <vector>    // for vector
 
 #include "aho-corasick-impl.hpp"  // for AhoCorasickImpl
-#include "multi-view.hpp"         // for MultiView
-#include "rewriters.hpp"          // for Rule
 
 namespace libsemigroups::detail {
 
-  ////////////////////////////////////////////////////////////////////////
-  // Overlap
-  ////////////////////////////////////////////////////////////////////////
-
-  // TODO move definition to cpp
-  struct Overlap {
-    Rule const* lhs;
-    Rule const* rhs;
-    size_t      length;
-
-    bool operator==(Overlap const& that) const {
-      return lhs == that.lhs && rhs == that.rhs && length == that.length;
-    }
-  };
+  // Forward decl
+  class Rule;
 
   ////////////////////////////////////////////////////////////////////////
   // OverlapIteratorTrie
   ////////////////////////////////////////////////////////////////////////
 
-  // TODO out of line all functions
   class OverlapIteratorTrie {
    public:
-    using iterator_category = std::input_iterator_tag;
+    struct Overlap {
+      Rule const* lhs;
+      Rule const* rhs;
+      size_t      length;
+
+      bool operator==(Overlap const& that) const {
+        return lhs == that.lhs && rhs == that.rhs && length == that.length;
+      }
+    };
+
+    using iterator_category = std::forward_iterator_tag;
     using value_type        = Overlap;
     using difference_type   = std::ptrdiff_t;
     using pointer           = value_type const*;
@@ -63,43 +56,24 @@ namespace libsemigroups::detail {
     using index_type           = typename Trie::index_type;
     using index_const_iterator = typename Trie::terminal_node_const_iterator;
 
-    index_const_iterator _current_word_iterator;
-    index_const_iterator _last_word_iterator;
-
-    index_type _word_index;
-    index_type _suffix_index;
-    index_type _suffix_descendent_index;
-
-    value_type _overlap;
-
-    Trie const* _trie;
-
+    index_const_iterator    _current_word_iterator;
+    index_const_iterator    _last_word_iterator;
     std::vector<index_type> _index_stack;
+    value_type              _overlap;
+    index_type              _suffix_index;
+    index_type              _suffix_descendent_index;
+    Trie const*             _trie;
+    index_type              _word_index;
 
    public:
-    OverlapIteratorTrie()
-        : _current_word_iterator(),
-          _last_word_iterator(),
-          _word_index(UNDEFINED),
-          _suffix_index(),
-          _suffix_descendent_index(),
-          _overlap(),
-          _trie(nullptr),
-          _index_stack() {};
+    OverlapIteratorTrie();
+    // TODO rule of five
 
-    // TODO: Use an init rather than setting default values?
-    OverlapIteratorTrie(Trie const& trie)
-        : _current_word_iterator(trie.cbegin_terminal_nodes()),
-          _last_word_iterator(trie.cend_terminal_nodes()),
-          _word_index(Trie::root),
-          _suffix_index(Trie::root),
-          _suffix_descendent_index(Trie::root),
-          _overlap(),
-          _trie(&trie),
-          _index_stack() {
-      _index_stack.reserve(trie.number_of_nodes());
-      operator++();
-    }
+    // TODO(1) init?
+
+    OverlapIteratorTrie(Trie const& trie);
+
+    ~OverlapIteratorTrie() = default;
 
     pointer operator->() const {
       return &_overlap;
@@ -110,47 +84,7 @@ namespace libsemigroups::detail {
     }
 
     // Pre-increment
-    // Each value in the range [_first, _last) points to a pair <index, rule>.
-    // For each of these pairs, we iteratively follow the suffix link, and
-    // then explore all of the descendants of that suffix link using DFS. We
-    // keep doing this until the suffix link is the root of the trie. Each
-    // descendant of each suffix link will correspond to a critical pair.
-    OverlapIteratorTrie& operator++() {
-      // Resume any partially-complete dfs
-      if (!_index_stack.empty()) {
-        if (find_next_descendent()) {
-          return *this;
-        }
-      }
-
-      // Resume any partially-complete travels to the root,
-      if (_suffix_index != Trie::root) {
-        _suffix_index = _trie->node_no_checks(_suffix_index).suffix_link();
-        if (traverse_to_root()) {
-          return *this;
-        }
-      }
-
-      // Start again with a new rule
-      while (_current_word_iterator != _last_word_iterator) {
-        _word_index = *_current_word_iterator;
-        ++_current_word_iterator;
-
-        auto const& node = _trie->node_no_checks(_word_index);
-
-        _overlap.lhs  = node.value();
-        _suffix_index = node.suffix_link();
-        LIBSEMIGROUPS_ASSERT(node.terminal());
-
-        if (traverse_to_root()) {
-          return *this;
-        }
-      }
-
-      // Indicates that we are finished.
-      _word_index = UNDEFINED;
-      return *this;
-    }
+    OverlapIteratorTrie& operator++();
 
     // Post-increment
     OverlapIteratorTrie operator++(int) {
@@ -171,42 +105,10 @@ namespace libsemigroups::detail {
 
    private:
     // TODO better name
-    bool traverse_to_root() {
-      while (_suffix_index != Trie::root) {
-        _index_stack.emplace_back(_suffix_index);
-        if (find_next_descendent()) {
-          return true;
-        }
-        _suffix_index = _trie->node_no_checks(_suffix_index).suffix_link();
-      }
-      return false;
-    }
+    [[nodiscard]] bool traverse_to_root();
 
     // Returns true if any descendent, and hence critical pair, is found.
-    bool find_next_descendent() {
-      while (!_index_stack.empty()) {
-        _suffix_descendent_index = _index_stack.back();
-        _index_stack.pop_back();
-
-        // Construct the critical pair
-        if (_trie->node_no_checks(_suffix_descendent_index).terminal()) {
-          _overlap.rhs
-              = _trie->node_no_checks(_suffix_descendent_index).value();
-          _overlap.length = _trie->node_no_checks(_suffix_index).height();
-          return true;
-        }
-
-        // Explore all children
-        for (letter_type x = 0; x < _trie->alphabet_size(); ++x) {
-          index_type child_index
-              = _trie->child_no_checks(_suffix_descendent_index, x);
-          if (child_index != UNDEFINED) {
-            _index_stack.emplace_back(child_index);
-          }
-        }
-      }
-      return false;
-    }
+    bool find_next_descendent();
   };  // class OverlapIteratorTrie
 }  // namespace libsemigroups::detail
 
